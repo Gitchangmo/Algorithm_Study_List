056dbd2e88d8a2e16e16a9314e85074865a85c61
changmo <333rlackdah@naver.com>
2025-10-14 23:46:49 +0900

    251014 백준 평범한 배낭 문제 해결

--- 파이썬/251014/백준_평범한 배낭(12865).py
+++ 파이썬/251014/백준_평범한 배낭(12865).py
@@ -1,17 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
+N, K = map(int, input().split()) # 물건 갯수와 최대 배낭 용량 입력
 
-items = []
+items = [(0,0)]     # 물건의 정보를 담을 리스트 생성 (튜플 형태로 저장)
 for i in range(N):
-    W, V = map(int, input().split())
-    items.append((W, V))
+    W, V = map(int, input().split())    # 물건정보 입력받음 무게, 가치
+    items.append((W, V))                # 튜플로 저장
 
-dp = [[0] * N for _ in range(K+1)]
+dp = [[0] * (K+1) for _ in range(N+1)]  # dp맵 생성 무게가 0일때와 물건정보 인덱스 맞추기 위해 각 +1 함
 
-for W, V in items:
-    print(W, V)
+for i in range(1, N+1): # 각 물건에 대해
+    for j in range(1, K+1): # # 배낭의 최대 용량까지
+        W, V = items[i] # 해당 물건의 무게와 가치를 저장
+        if j >= W:      # 해당 물건의 무게가 배낭의 최대용량보다 작거나 같으면
+            dp[i][j] = max(dp[i-1][j], V + dp[i-1][j-W])   # 물건 넣기 전 가치와 물건 넣었을 떄 가치 중 큰값 저장
+        else:
+            dp[i][j] =dp[i-1][j]    # 용량이 작아 못 넣으니 넣기 전과 동일함
+    
 
-for i in range(K+1):
-    print(dp[i])
+print(dp[N][K]) # 모든 물건을 다 확인한 후 최대 가치 값을 출력
